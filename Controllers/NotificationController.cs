5586f2a9c6354d82dc44441e0fba73fd24eb5fb1
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-11-20 15:23:16 -0700

    Add MVC Controllers  and created skeleton for them with  required methods;

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AZLearn.Models;
+
+namespace AZLearn.Controllers
+{
+    public class NotificationController :ControllerBase
+    {
+        public void CreateNotificationByHomeworkId(string StudentId){}
+       
+        public List<Notification> GetNotifications(string StudentId) { }
+
+
+        public void ArchiveNotificationsByStudentId(string StudentId) { }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AZLearn.Models;
+
+
+namespace AZLearn.Controllers
+{
+    public class UserController :ControllerBase
+    {
+        public User GetUserById(string UserId){ }
+
+   public void CreateUserByCohortId(string CohortId){
+   }
+   public  List<User> GetStudentsByCohortId(string CohortId){}
+    public void UpdateUserById(string UserId){}
+
+
+    public void ArchiveUserById(string UserId){}
+
+    }
+}
