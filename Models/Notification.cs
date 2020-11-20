19e54972fd5325a4e13c2ab9c07cada3ff4fd512
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-11-20 11:50:47 -0700

    Add Notification.cs Model;

---
+++ Models/Notification.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZLearn.Models
+{
+    [Table(nameof(Notification))]
+    public class Notification
+    {
+        [Key]
+        [Column(TypeName = "int(10)")]
+
+        /*Auto generates unique id number*/
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NotificationId { get; set; }
+
+        /*Foreign Key*/
+        [Required]
+        [Column(TypeName = "int(10)")]
+        public int StudentId { get; set; }
+
+        /*General Properties:*/
+        [Required]
+        [Column(TypeName = "varchar(250)")]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        [Column(TypeName = "boolean")]
+        public bool Archive { get; set; } = false;
+
+        /*   Navigation property:*/
+
+        [ForeignKey(nameof(StudentId))]
+        [InverseProperty(nameof(Models.User.Notifications))]
+        public virtual Student Student { get; set; }
+    }
+}
