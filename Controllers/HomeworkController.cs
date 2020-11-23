1e67eccd5e33d404e5a86742a42cf6fdb6db712b
Atinder-Pal <atty.pal46@gmail.com>
2020-11-22 23:36:24 -0700

    Created action GetHomeworksByCourseId in HomeworkController;

--- Controllers/HomeworkController.cs
+++ Controllers/HomeworkController.cs
@@ -10,6 +10,23 @@ namespace AZLearn.Controllers
 {
     public class HomeworkController : ControllerBase
     {
-
+        /// <summary>
+        /// This action takes in Course Id and Cohort Id and returns List of Homeworks for specified course under the specified Cohort Id
+        /// </summary>
+        /// <param name="courseId">Course Id</param>
+        /// <param name="cohortId">Cohort Id</param>
+        /// <returns>List of Homeworks for specified course under the specified Cohort Id</returns>
+        public static List<Homework> GetHomeworksByCourseId(string courseId, string cohortId)
+        {
+            List<Homework> homeworks;
+            int parsedCohortId = int.Parse(cohortId);
+            int parsedCourseId = int.Parse(courseId);
+            using (AppDbContext context = new AppDbContext())
+            {
+                homeworks = context.Homeworks
+                    .Where(key => key.CourseId == parsedCourseId && key.CohortId == parsedCohortId).ToList();
+            }
+            return homeworks;
+        }
     }
 }
