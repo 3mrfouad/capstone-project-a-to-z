7e840873af317300836a6a3461d83d97cb638c2d
Atinder-Pal <atty.pal46@gmail.com>
2020-11-22 23:41:10 -0700

    Created Action GetRubricsByHomeworkId in RubricController;

--- Controllers/RubricController.cs
+++ Controllers/RubricController.cs
@@ -10,6 +10,18 @@ namespace AZLearn.Controllers
 {
     public class RubricController :ControllerBase
     {
-
+        /// <summary>
+        /// This action takes in Homework Id and returns List of Rubrics associated with that Homework Id
+        /// </summary>
+        /// <param name="homeworkId">Homework Id</param>
+        /// <returns>List of Rubrics associated with specified Homework Id</returns>
+        public static List<Rubric> GetRubricsByHomeworkId(string homeworkId)
+        {
+            int parsedHomeworkId = int.Parse(homeworkId);
+            List<Rubric> rubrics = new List<Rubric>();
+            using var context = new AppDbContext();
+            rubrics = context.Rubrics.Where(key => key.HomeworkId == parsedHomeworkId).ToList();
+            return rubrics;
+        }
     }
 }
