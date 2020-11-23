bd1c33003c2d463d03526c53a3ed329131026ab1
Atinder-Pal <atty.pal46@gmail.com>
2020-11-22 23:45:09 -0700

    Created Action GetGradesByStudentId in GradeController;

--- Controllers/GradeController.cs
+++ Controllers/GradeController.cs
@@ -13,6 +13,21 @@ namespace AZLearn.Controllers
 {
     public class GradeController :ControllerBase
     {
-
+        /// <summary>
+        /// This Action takes in Student Id and Homework Id and returns List of Grades associated to that student in the specified Homework.
+        /// </summary>
+        /// <param name="studentId">Student Id</param>
+        /// <param name="homeworkId">Homework Id</param>
+        /// <returns>List of Grades associated with specified student for specified Homework</returns>
+        public static List<Grade> GetGradesByStudentId(string studentId, string homeworkId)
+        {
+            int parsedStudentId = int.Parse(studentId);
+            int parsedHomeworkId = int.Parse(homeworkId);
+            using var context = new AppDbContext();
+            var grades = context.Grades.Include("Rubric.Homework")
+                .Where(key => key.Rubric.HomeworkId == parsedHomeworkId && key.StudentId == parsedStudentId)
+                .ToList();
+            return grades;
+        }
     }
 }
