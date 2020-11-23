8160eadf01883b5ada671e54fa74464dfe2c665d
Atinder-Pal <atty.pal46@gmail.com>
2020-11-22 23:39:14 -0700

    Created Action GetStudentsByCohortId in UserController;

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,6 +12,18 @@ namespace AZLearn.Controllers
 {
     public class UserController :ControllerBase
     {
-       
+        /// <summary>
+        /// This Action takes in Cohort id and returns List of students enrolled in that Cohort.
+        /// </summary>
+        /// <param name="cohortId">Cohort Id</param>
+        /// <returns>List of students enrolled in specified Cohort</returns>
+        public static List<User> GetStudentsByCohortId(string cohortId)
+        {
+            int parsedCohortId = int.Parse(cohortId);
+            List<User> students = new List<User>();
+            using var context = new AppDbContext();
+            students = context.Users.Where(key => key.CohortId == parsedCohortId).ToList();
+            return students;
+        }
     }
 }
