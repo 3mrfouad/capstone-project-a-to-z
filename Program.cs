df31c2b257a9b15d76dcb73e6c82d8c0a3f6331b
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-11-22 18:03:34 -0700

    Built test action cases for CohortController and GradeController actions

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
+using AZLearn.Controllers;
+using AZLearn.Models;
 
 namespace AZLearn
 {
@@ -13,6 +16,59 @@ namespace AZLearn
     {
         public static void Main(string[] args)
         {
+            #region Testing Controllers' Actions
+
+
+
+            #region Create Cohort
+            /*CohortController.CreateCohort("Cohort 4.1", "20", "Edmonton", "Remote","2020-06-04","2020-08-30" );*/
+            /*Test passed*/
+            #endregion
+
+            #region Update Cohort
+            /*CohortController.UpdateCohortById("1", "Cohort 4.2", "40", "Edmonton", "Remote","2020-06-04","2020-08-30" );*/
+            /*Test passed*/
+            #endregion
+
+            #region Get Cohorts
+
+            /*var cohorts = CohortController.GetCohorts();
+            foreach (Cohort cohort in cohorts)
+            {
+                System.Diagnostics.Debug.WriteLine($"Id: {cohort.CohortId}  Mode of Teaching: {cohort.ModeOfTeaching}   Capacity: {cohort.Capacity} Name: {cohort.Name} City: {cohort.City} Start Date: {cohort.StartDate}  End Date: {cohort.EndDate}");
+            }*/
+            /*Test passed*/
+            #endregion
+
+            #region Create Grading by Student Id
+            /*var grading = new Dictionary<string, Tuple<string, string>>();
+            grading.Add("-1", new Tuple<string, string>("1", "Good Job"));
+            grading.Add("-2", new Tuple<string, string>("0", "Bad Job"));
+            GradeController.CreateGradingByStudentId("-1", grading);*/
+            /*Test Pass*/
+            #endregion
+
+
+            #region Update Grading by Student Id (overalload #1)
+            /*var grading = new Dictionary<string, Tuple<string, string>>();
+            grading.Add("-1", new Tuple<string, string>("1", "ok Job"));
+            grading.Add("-2", new Tuple<string, string>("1", "ok Job"));
+            GradeController.UpdateGradingByStudentId("-1", grading);*/
+            /*Test Pass*/
+            #endregion
+
+            #region Update Grading by Student Id (overalload #1)
+            var grading = new Dictionary<string,string>();
+            grading.Add("-1", "Thanks Instructor");
+            grading.Add("-2", "Thanks Instructor");
+            GradeController.UpdateGradingByStudentId("-1", grading);
+            /*Test Pass*/
+            #endregion
+
+            #endregion Testing Controllers' Actions
+
+            /*=============================================================================================================================*/
+
             CreateHostBuilder(args).Build().Run();
         }
 
