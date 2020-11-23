9f29612615efa024e5a5d838b4bfece37c01893b
Atinder-Pal <atty.pal46@gmail.com>
2020-11-23 14:01:47 -0700

    Created GetHomeworkSummary APIEndpoint ;

--- Controllers/ApplicationController_Atinder.cs
+++ Controllers/ApplicationController_Atinder.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AZLearn.Models;
 
 namespace AZLearn.Controllers
 {
@@ -11,5 +12,19 @@ namespace AZLearn.Controllers
     [ApiController]
     public class ApplicationController_Atinder :Controller
     {
+        /// <summary>     
+        ///     GetHomeworkSummary
+        ///     Request Type: GET
+        ///     This End point takes in Cohort Id and Course Id from global store and return List of homeworks associated with that Course for specified Cohort.
+        /// </summary>
+        /// <param name="courseId"></param>
+        /// <param name="cohortId"></param>
+        /// <returns>List Of Homeworks related to specified Course and Cohort</returns>
+        [HttpGet("HomeworkSummary")]
+        public ActionResult<IEnumerable<Homework>> GetHomeworkSummary(string courseId, string cohortId)
+        {
+            return HomeworkController.GetHomeworksByCourseId(courseId, cohortId);
+        }
+
     }
 }
