fff14f72f783ba092685962cdc67aa27b55d231a
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-11-22 18:02:50 -0700

    Built GradeController with CreateGradingByStudentId and two overload of UpdateGradingByStudentId controller actions

--- Controllers/GradeController.cs
+++ Controllers/GradeController.cs
@@ -3,23 +3,80 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AZLearn.Data;
 using AZLearn.Models;
 
 namespace AZLearn.Controllers
 {
-    public class GradeController :ControllerBase
+    public class GradeController : ControllerBase
     {
-        public void CreateGradeByRubricId(string RubricId)
+
+        public List<Grade> GetGrades(string homeworkId)
+        {
+            return new List<Grade>();
+        }
+        public void GetGradeByRubricId(string rubricId)
         {
         }
-
-       public List<Grade> GetGrades(string HomeworkId)
+        /// <summary>
+        ///     CreateGradingByStudentId
+        ///     Description: Controller action that creates new gradings for a student
+        ///     It expects below parameters, and would populate the students grades for a specific rubric / homework
+        /// </summary>
+        /// <param name="studentId">string provided from frontend, and parsed to int to match model property data type</param>
+        /// <param name="gradings">Dictionary with rubricId as key and Tuple as value, the Tuple has two parameters i.e., mark and instructor comment</param>
+        public static void CreateGradingByStudentId(string studentId, Dictionary<string, Tuple<string, string>> gradings)
         {
+            using var context = new AppDbContext();
+            foreach (var (rubricId, (mark, instructorComment)) in gradings)
+            {
+                context.Grades.Add(new Grade()
+                {
+                    RubricId = int.Parse(rubricId),
+                    StudentId = int.Parse(studentId),
+                    Mark = int.Parse(mark),
+                    InstructorComment = instructorComment
+                });
+            }
+            context.SaveChanges();
         }
-
-       public void UpdateGradeById(string GradeId){}
-
-       public void ArchiveGradesByHomeworkId(string HomeworkId){}
+        /// <summary>
+        ///     UpdateGradingByStudentId
+        ///     Description: Controller action that updates new gradings for a student i.e., grades and instructor comments
+        ///     It expects below parameters, and would populate the updated students grades for a specific rubric / homework
+        /// </summary>
+        /// <param name="studentId">string provided from frontend, and parsed to int to match model property data type</param>
+        /// <param name="gradings">Dictionary with rubricId as key and Tuple as value, the Tuple has two parameters i.e., mark and instructor comment</param>
+        public static void UpdateGradingByStudentId(string studentId, Dictionary<string, Tuple<string, string>> gradings)
+        {
+            using var context = new AppDbContext();
+            var grade = new Grade(); ;
+            foreach (var (rubricId, (mark, instructorComment)) in gradings)
+            {
+                grade = context.Grades.Find(int.Parse(rubricId), int.Parse(studentId));
+                grade.Mark = int.Parse(mark);
+                grade.InstructorComment = instructorComment;
+            }
+            context.SaveChanges();
+        }
+        /// <summary>
+        /// UpdateGradingByStudentId
+        ///     Description: Controller action that updates new gradings for a student i.e., student comments
+        ///     It expects below parameters, and would populate the updated student comments for a specific rubric / homework
+        /// </summary>
+        /// <param name="studentId"></param>
+        /// <param name="studentComment">Dictionary with rubricId as key and studentComment as value</param>
+        public static void UpdateGradingByStudentId(string studentId, Dictionary<string, string> studentComment)
+        {
+            using var context = new AppDbContext();
+            var grade = new Grade(); ;
+            foreach (var (rubricId, comment) in studentComment)
+            {
+                grade = context.Grades.Find(int.Parse(rubricId), int.Parse(studentId));
+                grade.StudentComment = comment;
+            }
+            context.SaveChanges();
+        }
 
 
     }
