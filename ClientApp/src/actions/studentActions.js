45e12425b0457bc1c2ce7c32b32649d08da683d7
Alex Liu <liuzhendell@gmail.com>
2020-11-26 10:52:43 -0700

    add reducer function in student part

--- ClientApp/src/actions/studentActions.js
+++ ClientApp/src/actions/studentActions.js
@@ -21,3 +21,79 @@ export const courseSummaryStudent = () => {
     }
   };
 };
+
+export const homeworkSummaryStudent = () => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "HOMEWORK_SUMMARY_STUDENT_REQUEST" });
+      // update the url later
+      const { data } = await axios.get("/api/products");
+      dispatch({
+        type: "HOMEWORK_SUMMARY_STUDENT_SUCCESS",
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "HOMEWORK_SUMMARY_STUDENT_FAIL",
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.response,
+      });
+    }
+  };
+};
+
+export const homeworkStudent = () => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "HOMEWORK_STUDENT_REQUEST" });
+      // update the url later
+      const { data } = await axios.get("/api/products");
+      dispatch({
+        type: "HOMEWORK_STUDENT_SUCCESS",
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "HOMEWORK_STUDENT_FAIL",
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.response,
+      });
+    }
+  };
+};
+
+export const createTimeSheetStudent = (timeSheet) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: "CREATE_TIME_SHEET_STUDENT_REQUEST",
+      });
+      //   const {
+      //     userLogin: { userInfo },
+      //   } = getState();
+      //   const config = {
+      //     headers: {
+      //       Authorization: `Bearer ${userInfo.token}`,
+      //     },
+      //   };
+      const { data } = await axios.post(`/api/products`, timeSheet, config);
+
+      dispatch({
+        type: "CREATE_TIME_SHEET_STUDENT_SUCCESS",
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "CREATE_TIME_SHEET_STUDENT_FAIL",
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.response,
+      });
+    }
+  };
+};
--- ClientApp/src/reducers/studentReducer.js
+++ ClientApp/src/reducers/studentReducer.js
@@ -15,3 +15,53 @@ export const courseSummaryStudentReducer = (
       return state;
   }
 };
+
+export const homeworkSummaryStudentReducer = (
+  state = { homework: [] },
+  action
+) => {
+  switch (action.type) {
+    case "HOMEWORK_SUMMARY_STUDENT_REQUEST":
+      return { loading: true, homework: [] };
+    case "HOMEWORK_SUMMARY_STUDENT_SUCCESS":
+      return { loading: false, homework: action.payload };
+
+    case "HOMEWORK_SUMMARY_STUDENT_FAIL":
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const homeworkStudentReducer = (state = { homework: {} }, action) => {
+  switch (action.type) {
+    case "HOMEWORK_STUDENT_REQUEST":
+      return { loading: true, homework: {} };
+    case "HOMEWORK_STUDENT_SUCCESS":
+      return { loading: false, homework: action.payload };
+
+    case "HOMEWORK_STUDENT_FAIL":
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const createTimeSheetStudentReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "CREATE_TIME_SHEET_STUDENT_REQUEST":
+      return { loading: true };
+    case "CREATE_TIME_SHEET_STUDENT_SUCCESS":
+      return { loading: false, success: true, timeSheet: action.payload };
+
+    case "CREATE_TIME_SHEET_STUDENT_FAIL":
+      return { loading: false, error: action.payload };
+    case "CREATE_TIME_SHEET_STUDENT_RESET":
+      return {};
+
+    default:
+      return state;
+  }
+};
