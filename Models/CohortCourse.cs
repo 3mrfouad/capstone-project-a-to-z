17e048553f289d827c269833abc219bfa4f0d986
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-12-01 23:38:52 -0700

    Advanced FormatCohortcourse model

--- Models/CohortCourse.cs
+++ Models/CohortCourse.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AZLearn.Models
 {
@@ -11,32 +8,29 @@ namespace AZLearn.Models
     public class CohortCourse
     {
         /* Composite Key with CohortId, CourseId */
-        [Key, Column(Order = 0, TypeName = "int(10)")]
+
+        [Key]
+        [Column(Order = 0, TypeName = "int(10)")]
         public int CohortId { get; set; }
 
-        [Key, Column(Order = 1, TypeName = "int(10)")]
+        [Key]
+        [Column(Order = 1, TypeName = "int(10)")]
         public int CourseId { get; set; }
-
 
         [Required]
         [Column(TypeName = "int(10)")]
         public int InstructorId { get; set; }
 
-        [Required]
-        [Column(TypeName = "date")]
-        public DateTime StartDate { get; set; }
+        [Required] [Column(TypeName = "date")] public DateTime StartDate { get; set; }
 
-        [Required]
-        [Column(TypeName = "date")]
-        public DateTime EndDate { get; set; }
+        [Required] [Column(TypeName = "date")] public DateTime EndDate { get; set; }
 
-        [Column(TypeName = "varchar(250)")]
-        public string ResourcesLink { get; set; }
+        [Column(TypeName = "varchar(250)")] public string ResourcesLink { get; set; }
 
-        [Column(TypeName = "boolean")]
-        public bool Archive { get; set; } = false;
+        [Column(TypeName = "boolean")] public bool Archive { get; set; } = false;
 
         /* Navigation Properties */
+
         [InverseProperty(nameof(Models.Cohort.CohortCourses))]
         public virtual Cohort Cohort { get; set; }
 
@@ -44,7 +38,7 @@ namespace AZLearn.Models
         public virtual Course Course { get; set; }
 
         [ForeignKey(nameof(InstructorId))]
-        [InverseProperty(nameof(Models.User.CohortCourses))]
+        [InverseProperty(nameof(User.CohortCourses))]
         public virtual User Instructor { get; set; }
     }
 }
