88150e6739db5b76fdab4c66597c3ea9d7c37f7d
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-12-01 23:37:48 -0700

    Advanced Format Cohort Models

--- Models/Cohort.cs
+++ Models/Cohort.cs
@@ -14,6 +14,7 @@ namespace AZLearn.Models
             Users = new HashSet<User>();
             CohortCourses = new HashSet<CohortCourse>();
         }
+
         [Key]
         [Column(TypeName = "int(10)")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -23,36 +24,30 @@ namespace AZLearn.Models
         [Column(TypeName = "varchar(50)")]
         public string Name { get; set; }
 
-        [Column(TypeName = "int(3)")]
-        public int? Capacity { get; set; }
+        [Column(TypeName = "int(3)")] public int? Capacity { get; set; }
 
         [Required]
         [Column(TypeName = "varchar(50)")]
         public string ModeOfTeaching { get; set; }
 
-        [Required]
-        [Column(TypeName = "date")]
-        public DateTime StartDate { get; set; }
+        [Required] [Column(TypeName = "date")] public DateTime StartDate { get; set; }
 
-        [Required]
-        [Column(TypeName = "date")]
-        public DateTime EndDate { get; set; }
+        [Required] [Column(TypeName = "date")] public DateTime EndDate { get; set; }
 
         [Required]
         [Column(TypeName = "varchar(50)")]
         public string City { get; set; }
 
-        [Column(TypeName = "boolean")]
-        public bool Archive { get; set; } = false;
+        [Column(TypeName = "boolean")] public bool Archive { get; set; } = false;
 
         /*Navigation Properties*/
-        [InverseProperty(nameof(Models.CohortCourse.Cohort))]
+
+        [InverseProperty(nameof(CohortCourse.Cohort))]
         public virtual ICollection<CohortCourse> CohortCourses { get; set; }
 
-        [InverseProperty(nameof(Models.User.Cohort))]
-        public virtual ICollection<User> Users { get; set; }
+        [InverseProperty(nameof(User.Cohort))] public virtual ICollection<User> Users { get; set; }
 
-        [InverseProperty(nameof(Models.Homework.Cohort))]
+        [InverseProperty(nameof(Homework.Cohort))]
         public virtual ICollection<Homework> Homeworks { get; set; }
     }
 }
