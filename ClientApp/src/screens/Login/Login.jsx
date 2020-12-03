5b766112666d5f37e3bc36f4c46d94f757e9220d
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-12-03 09:03:12 -0700

    Add Login details validataion at front end

--- ClientApp/src/screens/Login/Login.jsx
+++ ClientApp/src/screens/Login/Login.jsx
@@ -5,39 +5,77 @@ import { Link, useHistory } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [password, setPassword] = useState("");
+    const [validated, setValidated] = useState(false);
+    const [invalidPasswordBL, setInvalidPasswordBl] = useState(false);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+    let input = {};
+    input["password"] = "";
+    let errors = {};
+
+    const submitHandler = (e) => {
+        //(2) Add form validation condition block if-else
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        setValidated(true);
+        e.preventDefault();
     // dispatch(login(email, password));
-    console.log("login");
-  };
+        console.log("login");
+        //Business Logic
+        if (password === ("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")) {
+            e.preventDefault();
+            password ? setInvalidPasswordBl(true) : setInvalidPasswordBl(false);
+        } else {
+            setInvalidPasswordBl(false);
+            e.preventDefault();
+
+        }
+     /*   if (typeof input["password"] !== "undefined") {
+
+            var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+            if (!pattern.test(input["password"])) {
+                isValid = false;
+                errors["password"] = "Please enter valid Password.";
+            }
+        }*/
+    };
   return (
-    <>
+    <React.Fragment>
       <Container>
         <Row className="justify-content-md-center">
           <Col xs={12} md={6}>
             <h1>Sign In</h1>
             {/* {error && <Message variant="danger">{error}</Message>}
             {loading && <Loader />} */}
-            <Form onSubmit={submitHandler}>
+                      <Form noValidate validated={validated} onSubmit={submitHandler}>
               <Form.Group controlId="email">
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control
-                  type="email"
+                                  required
+                                  type="email"
+                                  maxlength="50"
                   placeholder="Enter Email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                ></Form.Control>
+                              ></Form.Control>
+                  <Form.Control.Feedback type="invalid">
+                      Please enter an email address.
+                  </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control
+                              <Form.Control
+                                  required
                   type="password"
-                  placeholder="Enter Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                                  placeholder="Enter Password"
+                                  name="password"
+                                  value={password}
+                                  maxLength="250"
+                                  onChange={(e) => setPassword(e.target.value)}
                 ></Form.Control>
               </Form.Group>
               <Button type="submit" variant="primary">
@@ -53,7 +91,7 @@ const Login = () => {
           </Col>
         </Row> */}
       </Container>
-    </>
+      </React.Fragment>
   );
 };
 
