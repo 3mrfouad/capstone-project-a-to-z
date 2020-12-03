51607761d03d530649ce1d63976cd95eb7ca7948
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-12-03 09:02:03 -0700

    Add the validation for Cohort Edit front end

--- ClientApp/src/components/Instructor/CohortEdit/CohortEdit.jsx
+++ ClientApp/src/components/Instructor/CohortEdit/CohortEdit.jsx
@@ -13,6 +13,8 @@ const CohortEdit = ({ match, history }) => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [city, setCity] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [invalidDatesBL, setInvalidDatesBl] = useState(false);
   const dispatch = useDispatch();
 
   const cohortEdit = useSelector((state) => state.cohortEdit);
@@ -21,17 +23,18 @@ const CohortEdit = ({ match, history }) => {
   const { cohort, loading } = cohortGetState;
 
   useEffect(() => {
-    if (!cohort || !cohort.name) {
-      dispatch(cohortGet(cohortId));
-    } else {
-      setName(cohort.name);
-      setCapacity(cohort.capacity);
-      setCity(cohort.city);
-      setEndDate(cohort.endDate);
-      setStartDate(cohort.startDate);
-      setModeOfTeaching(cohort.modeOfTeaching);
-    }
-  }, [dispatch, cohort]);
+      if (!cohort || !cohort.name) {
+        dispatch(cohortGet(cohortId));
+      } else {
+        setName(cohort.name);
+        setCapacity(cohort.capacity);
+        setCity(cohort.city);
+        setEndDate(cohort.endDate);
+        setStartDate(cohort.startDate);
+        setModeOfTeaching(cohort.modeOfTeaching);
+      }
+    },
+    [dispatch, cohort]);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -39,92 +42,150 @@ const CohortEdit = ({ match, history }) => {
     // this is where we need validation
     // the dispatch call will be conditioned  if else statement or equivalent
 
-    console.log("update cohort");
-    dispatch(
-      editCohort({
-        cohortId,
-        name,
-        capacity,
-        city,
-        modeOfTeaching,
-        startDate,
-        endDate,
-      })
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    setValidated(true);
+
+    //(3) Add business logic
+    if (endDate === "" ||
+        startDate === "" ||
+        Date.parse(endDate) < Date.parse(startDate)) {
+        e.preventDefault();
+        Date.parse(endDate) < Date.parse(startDate)
+            ? setInvalidDatesBl(true)
+            : setInvalidDatesBl(false);
+        setEndDate("");
+    }
+    else
+    {
+
+        setInvalidDatesBl(false);
+        e.preventDefault();
+        console.log("update cohort");
+        dispatch(
+            editCohort({
+                cohortId,
+                name,
+                capacity,
+                city,
+                modeOfTeaching,
+                startDate,
+                endDate,
+            })
+        );
+    }
+  };
+    return (
+      <React.Fragment>
+        {loading
+          ? (<Loader/>)
+          : (<Container>
+               <Row className="justify-content-md-center">
+                 { /* check the pagination */
+                 }
+                 <Col xs={12} md={6}>
+                   <h2>Cohort</h2>
+                   <Form noValidate validated={validated} onSubmit={
+submitHandler}>
+                     <Form.Group controlId="name">
+                       <Form.Label>Cohort Name</Form.Label>
+                       <Form.Control
+                         required
+                                        type="text"
+                         maxlength="50"
+                         value={name}
+                         onChange={(e) => setName(e.target.value)
+}>
+                       </Form.Control>
+                       <Form.Control.Feedback type="invalid">
+                         Please enter a cohort name.
+                       </Form.Control.Feedback>
+                     </Form.Group>
+                     <Form.Group controlId="Capacity">
+                       <Form.Label>Capacity</Form.Label>
+                       <Form.Control
+                         type="number"
+                         min={0}
+                         max={999}
+                         step="1"
+
+                         value={capacity}
+                         onChange={(e) => setCapacity(String(e.target.value))
+}>
+                       </Form.Control>
+                     </Form.Group>
+                     <Form.Group controlId="Mode of Teaching">
+                       <Form.Label>Mode of Teaching</Form.Label>
+                       <Form.Control
+                         as="select"
+                         required
+                         value={modeOfTeaching}
+                         onChange={(e) => setModeOfTeaching(
+                           String(e.target.value))}>
+                         <option></option>
+                         <option>Online</option>
+                         <option>In Person</option>
+                       </Form.Control>
+                     </Form.Group>
+                     <Form.Group controlId="Start Date">
+                       <Form.Label>Start Date</Form.Label>
+                       <Form.Control
+                         required
+                         type="date"
+                         value={startDate}
+                         onChange={(e) => setStartDate(String(e.target.value))
+}>
+                       </Form.Control>
+                       <Form.Control.Feedback type="invalid">
+                         Please choose a start date.
+                       </Form.Control.Feedback>
+                     </Form.Group>
+                     <Form.Group controlId="End Date">
+                       <Form.Label>End Date</Form.Label>
+                       <Form.Control
+                         required
+                         type="date"
+                         value={endDate}
+                         onChange={(e) => setEndDate(String(e.target.value))
+}>
+                       </Form.Control>
+                       <Form.Control.Feedback type="invalid">
+                         Please choose an end date.
+                       </Form.Control.Feedback>
+                       <p className="text-danger small">{invalidDatesBL
+                         ? "End date can't be before start date"
+                         : ""}</p>
+                     </Form.Group>
+                     <Form.Group controlId="City">
+                       <Form.Label>City</Form.Label>
+                       <Form.Control
+                         as="select"
+                         required
+                         value={city}
+                         onChange={(e) => setCity(e.target.value)}>
+                         <option></option>
+                         <option>Edmonton</option>
+                         <option>Calgary</option>
+                         <option>Other</option>
+                       </Form.Control>
+                       <Form.Control.Feedback type="invalid">
+                         Please choose a city.
+                       </Form.Control.Feedback>
+                     </Form.Group>
+                     <a href="">Back</a>
+                     <Button type="submit" variant="primary" className="float-right">
+                       {" "}
+                       Save
+                     </Button>
+                   </Form>
+                 </Col>
+               </Row>
+             </Container>)}
+      </React.Fragment>
     );
   };
-  return (
-    <React.Fragment>
-      {loading ? (
-        <Loader />
-      ) : (
-        <Container>
-          <Row className="justify-content-md-center">
-            {/* check the pagination */}
-            <Col xs={12} md={6}>
-              <h2>Cohort</h2>
-              <Form onSubmit={submitHandler}>
-                <Form.Group controlId="name">
-                  <Form.Label>Cohort Name</Form.Label>
-                  <Form.Control
-                    required
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId="Capacity">
-                  <Form.Label>Capacity</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={capacity}
-                    onChange={(e) => setCapacity(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="Mode of Teaching">
-                  <Form.Label>Mode of Teaching</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={modeOfTeaching}
-                    onChange={(e) => setModeOfTeaching(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="Start Date">
-                  <Form.Label>Start Date</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="End Date">
-                  <Form.Label>End Date</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-                <Form.Group controlId="City">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-                <a href="">Back</a>
-                <Button type="submit" variant="primary" className="float-right">
-                  {" "}
-                  Save
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      )}
-    </React.Fragment>
-  );
-};
-
-export default CohortEdit;
+  export default
+  CohortEdit;;
