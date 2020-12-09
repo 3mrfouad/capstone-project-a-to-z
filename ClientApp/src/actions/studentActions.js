09b03661c62d3abb80fa06226e5b46b4e87b9244
Alex Liu <liuzhendell@gmail.com>
2020-12-09 01:32:06 -0700

    update

--- ClientApp/src/actions/studentActions.js
+++ ClientApp/src/actions/studentActions.js
@@ -5,7 +5,6 @@ export const courseSummaryStudent = () => {
   return async (dispatch) => {
     try {
       dispatch({ type: "COURSE_SUMMARY_STUDENT_REQUEST" });
-      // update the url later
       const {
         data,
       } = await axios.get(
@@ -32,7 +31,6 @@ export const homeworkSummaryStudent = (id) => {
   return async (dispatch) => {
     try {
       dispatch({ type: "HOMEWORK_SUMMARY_STUDENT_REQUEST" });
-      // update the url later
       const {
         data,
       } = await axios.get(
@@ -59,7 +57,6 @@ export const homeworkStudent = (id) => {
   return async (dispatch) => {
     try {
       dispatch({ type: "HOMEWORK_STUDENT_REQUEST" });
-      // update the url later
       const { data } = await axios.get(
         "https://localhost:5001/application/gethomework",
         {
@@ -117,14 +114,6 @@ export const createTimeSheetStudent = (solvingHrs, studyHrs) => {
       dispatch({
         type: "CREATE_TIME_SHEET_STUDENT_REQUEST",
       });
-      //   const {
-      //     userLogin: { userInfo },
-      //   } = getState();
-      //   const config = {
-      //     headers: {
-      //       Authorization: `Bearer ${userInfo.token}`,
-      //     },
-      //   };
       const { data } = await axios.post(
         "https://localhost:5001/application/createtimesheet",
 
@@ -165,14 +154,6 @@ export const updateTimeSheetStudent = (timesheetId, solvingHrs, studyHrs) => {
       dispatch({
         type: "UPDATE_TIME_SHEET_STUDENT_REQUEST",
       });
-      //   const {
-      //     userLogin: { userInfo },
-      //   } = getState();
-      //   const config = {
-      //     headers: {
-      //       Authorization: `Bearer ${userInfo.token}`,
-      //     },
-      //   };
       const params = {
         timesheetId: timesheetId,
         solvingTime: solvingHrs,
